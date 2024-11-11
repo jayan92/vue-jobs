0b263f6ebb2c5126b983589fbde2c1718e042075
Jayan <mail4jayanchinthaka@gmail.com>
2024-11-11 06:57:29 +0530

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <Hero
+    title="Become a Vue Dev"
+    subtitle="Find the Vue job that fits your skills and needs"
+  />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
+</template>
+
+<script setup>
+import Hero from "@/components/Hero.vue";
+import HomeCards from "@/components/HomeCards.vue";
+import JobListings from "@/components/JobListings.vue";
+</script>
